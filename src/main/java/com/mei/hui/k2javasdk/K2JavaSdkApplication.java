b67d8aff9681a64a5f460ca0f76bba5c8e4c5700
shangbin@meihuitech.com <shangbin@meihuitech.com>
2021-07-08 14:53:02 +0800

    first commit

---
+++ src/main/java/com/mei/hui/k2javasdk/K2JavaSdkApplication.java
@@ -0,0 +1,13 @@
+package com.mei.hui.k2javasdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2JavaSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K2JavaSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mei/hui/k2javasdk/K2JavaSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mei.hui.k2javasdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2JavaSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
